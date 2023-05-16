104c5467e94715fb427be11a4f8693e00c6b27ab
mrdulin <novaline.dulin@gmail.com>
2023-05-16 16:06:34 +0800

    feat: update example

--- stackoverflow/76254300/models/post.ts
+++ stackoverflow/76254300/models/post.ts
@@ -12,7 +12,8 @@ PostSchema.pre('remove', async function () {
   console.log('this.comments: ', this.comments);
   const check = this.comments.map(c => c instanceof mongoose.Types.ObjectId)
   console.log(check);
-  await this.model('comment').deleteMany({ _id: { $in: this.comments } })
+  // await this.model('comment').deleteMany({ _id: { $in: this.comments } })
+  await this.model('comment').deleteMany({ _id: this.comments })
 })
 
 export default mongoose.model('post', PostSchema);
