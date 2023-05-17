4fb37ef987a70a52ea404bb62996825bce6ee02c
mrdulin <novaline.dulin@gmail.com>
2023-05-17 21:31:11 +0800

    feat: add push element to nested array of a document

---
+++ stackoverflow/75779709/main.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import { config } from '../../src/config';
+import util from 'util';
+
+const schema = new mongoose.Schema({
+  name: String,
+  topics: [{ name: String, responses: [{ name: String }] }]
+});
+
+const Model = mongoose.model('model', schema);
+
+(async function main() {
+  try {
+    await mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+    const docs = await Model.create([
+      { name: 'name a', topics: [{ name: 'topic a', responses: [{ name: 'response a' }] }, { name: 'topic b', responses: [] }] },
+      { name: 'name b', topics: [{ name: 'topic b', responses: [{ name: 'response x' }] }] }
+    ]);
+
+    const targetDocId = docs[0]._id;
+    const targetTopicId = docs[0].topics[0]._id;
+
+    const updatedDoc = await Model.findOneAndUpdate(
+      {
+        _id: targetDocId,
+        'topics._id': targetTopicId
+      },
+      {
+        $push: { 'topics.$.responses': { name: 'response b' } }
+      },
+      { new: true }
+    )
+
+    console.log('updatedDoc: ', util.inspect(updatedDoc, false, null))
+
+  } catch (error) {
+    console.error(error);
+  } finally {
+    // await mongoose.connection.dropCollection('models');
+    await mongoose.connection.close()
+  }
+})();
