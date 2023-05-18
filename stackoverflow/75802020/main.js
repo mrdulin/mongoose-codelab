0247b80b05544265b76c25f907291a9786a12612
mrdulin <novaline.dulin@gmail.com>
2023-05-18 15:56:55 +0800

    feat: add mongoose map example

---
+++ stackoverflow/75802020/main.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import { config } from '../../src/config';
+const { Schema } = mongoose;
+
+mongoose.set('debug', true);
+
+const chatSchema = new Schema({
+  users: {
+    type: Map,
+    of: new Schema({
+      createdAt: { type: Date, default: Date.now, immutable: true },
+      role: { type: String, default: 'guest', required: true },
+    }),
+    required: true,
+    default: new Map(),
+  }
+});
+
+const Chat = mongoose.model('chat', chatSchema);
+
+(async function main() {
+  try {
+    await mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+    const chat = new Chat();
+    chat.users.set(new mongoose.Types.ObjectId(), { role: 'admin' })
+    await chat.save();
+
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await mongoose.connection.close()
+  }
+})();
