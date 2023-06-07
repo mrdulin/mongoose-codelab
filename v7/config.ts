18bdb552595156a23e94df72e8402113839be45f
official_dulin <novaline.dulin@gmail.com>
2023-06-07 14:08:56 +0000

    chore: setup codespaces environment variables

--- v7/config.ts
+++ v7/config.ts
@@ -1,12 +1,16 @@
-const dotenv = require('dotenv');
-const path = require('path');
+if (!process.env.CODESPACES) {
+  const dotenv = require('dotenv');
+  const path = require('path');
 
-const result = dotenv.config({ path: path.resolve(__dirname, '../.env') });
-if (result.error) {
-  console.error(result.error);
-  process.exit(1);
+  const result = dotenv.config({ path: path.resolve(__dirname, '../.env') });
+  if (result.error) {
+    console.error(result.error);
+    process.exit(1);
+  }
+  console.log(result.parsed);
 }
-console.log(result.parsed);
+
+console.log(process.env)
 
 const config = {
   MONGODB_URI: process.env.MONGODB_URI || '',
