80f4060e8f3d7c254a1f51c0cc53bade1751738e
mrdulin <novaline.dulin@gmail.com>
2023-06-30 19:36:07 +0800

    feat: the order calling sort and limit doesn't matter

---
+++ v7/stackoverflow/76031707/index.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import { config } from '../../config';
+
+mongoose.set('debug', true);
+
+const userSchema = new mongoose.Schema({
+	name: String,
+});
+const User = mongoose.model('user', userSchema);
+
+(async function main() {
+	try {
+		await mongoose.connect(config.MONGODB_URI);
+		// await Promise.all([Record].map((m) => m.collection.drop()));
+		// seed
+		// const users = await User.find({}).limit(2).sort({ _id: 'desc' }).exec();
+		const filter = {};
+		const r = await User.aggregate().match({}).limit(2).sort({ _id: 'desc' }).exec();
+
+		console.log(r);
+	} catch (error) {
+		console.error(error);
+	} finally {
+		await mongoose.connection.close();
+	}
+})();
---
+++ v7/stackoverflow/76587459/index.ts
@@ -0,0 +1,30 @@
+// @ts-nocheck
+import mongoose from 'mongoose';
+import { config } from '../../config';
+
+mongoose.set('debug', true);
+
+const schema = new mongoose.Schema({
+	currentMovie: [{ lang: String, movie: String }],
+});
+const Account = mongoose.model('account', schema);
+
+(async function main() {
+	try {
+		await mongoose.connect(config.MONGODB_URI);
+		await Promise.all([Account].map((m) => m.collection.drop()));
+		// seed
+		const a = new Account({ currentMovie: [{ lang: 'en', movie: 'Zootopia 2016' }] });
+		await a.save();
+
+		// test
+		const account = await Account.findOne();
+		const currentMovie = account.currentMovie.find((m) => m.lang == 'en');
+		currentMovie.movie = 'Oblivion 2013';
+		await account?.save();
+	} catch (error) {
+		console.error(error);
+	} finally {
+		await mongoose.connection.close();
+	}
+})();
